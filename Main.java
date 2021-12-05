f4de0569d22ab502804cca86cffbd3ed0c9fe275
itsFabiDev <44497089+itsFabiDev@users.noreply.github.com>
2021-12-05 23:02:49 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,14 +11,21 @@ public class Main {
 
 }
 
+class Buchstaben {
+    static String[] test = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k","l", "m", "n", "o", "p", "q", "r", "s","t","u","v","w","x","y","z"};
+}
+
 class Scheiße {
     static String scheiße = "";
     public Scheiße(String scheiße) {
         this.scheiße = scheiße;
     }
 
-    public void gibaus() {
-        Print.print("Scheiße");
+    public static void gibaus() {
+        String[] echtertest = Buchstaben.test;
+        String ausrufezeichen1 = "!";
+        char ausrufezeichen = ausrufezeichen1.charAt(0);
+        Print.print(echtertest[18]+echtertest[2]+echtertest[7]+echtertest[4]+echtertest[8]+echtertest[18]+echtertest[18]+echtertest[4]+ausrufezeichen);
     }
 
 }
@@ -35,7 +42,7 @@ class Schleife {
 
     public static void Schleife() {
         while(Scheiße.scheiße == "Scheiße!"){
-            Print.print("Scheiße!");
+            Scheiße.gibaus();
         }
     }
 
